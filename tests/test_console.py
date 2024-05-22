43a8c689559cd3524fba3fd276fe6e9ae2084d3e
Zane Malik <zmalik1@student.hult.edu>
2024-05-22 13:56:54 -0400

    Added support for multiple languages Added support for multiple platforms

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,18 +1,21 @@
 import click.testing
 import pytest
+import requests
 
 @pytest.fixture
 def runner():
     return click.testing.CliRunner()
 
 @pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch("requests.get")
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem ipsum dolor sit amet",
-    }
-    return mock
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("wikiapp.wikipedia.random_page")
+
+#def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+ #   runner.invoke(console.main, ["--language","es"])
+  #  print(">>>", mock_wikipedia_random_page.call_args)
+   # mock_wikipedia_random_page.assert_called_with("en")
+
+
 
 from wikiapp import console
 
@@ -27,12 +30,18 @@ def test_main_invokes_requests_get(runner, mock_requests_get):
 
 def test_main_uses_correct_url(runner, mock_requests_get):
     result = runner.invoke(console.main)
+    print("<<<", mock_requests_get.call_args)
     assert mock_requests_get.call_args[0] == ("https://en.wikipedia.org/api/rest_v1/page/random/summary",)
 
 def test_main_fails_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = Exception("Boom")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
+
+def test_main_prints_message_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.RequestException
+    result = runner.invoke(console.main)
+    assert "Error" in result.output
 
 
 def test_main_succeeds(runner):
--- wikiapp/console.py
+++ wikiapp/console.py
@@ -6,15 +6,22 @@ import textwrap
 from wikiapp.wikipedia import API_URL, random_page
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en", 
+    help="Gets random page from Wikipedia in given language",
+    metavar="LANG",
+    show_default=True, 
+)
 @click.version_option(version=__version__)
-def main():
+def main(language="en"):
     """The ultramodern Python project."""
-    with requests.get(API_URL) as response:
-        data = random_page()
-        title= data["title"]
-        extract = data["extract"]
-        click.secho(title, fg="green")
-        click.echo(textwrap.fill(extract))
+         data = random_page(language=language)
+            title= data["title"]
+            extract = data["extract"]
+            click.secho(title, fg="green")
+            click.echo(textwrap.fill(extract))
 
 if __name__=="__main__":
     main()
