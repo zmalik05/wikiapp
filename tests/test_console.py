61cc35898e785e94a017a3a531c04bde977c00c5
Zane Malik <zmalik1@student.hult.edu>
2024-05-22 10:34:38 -0400

    Bruh

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,8 +1,41 @@
 import click.testing
+import pytest
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests, get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
 
 from wikiapp import console
 
-def test_main_succeeds():
+def test_main_prints_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
+
+
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert mock_requests_get.called
+
+def test_main_uses_correct_url(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert mock_requests_get.call_args[0] = ("https://en.wikipedia.org/api/rest_v1/page/random/summary",)
+
+def test_main_fails_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = Exception("Boom")
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
+
+
+def test_main_succeeds(runner):
     runner = click.testing.CliRunner()
     result = runner.invoke(console.main)
-    assert result.exit.code == 0
+    assert result.exit_code == 0
